f876b03e65ab5096769d88100610d21054dcb503
Dan Hemerlein <danhemerlein@gmail.com>
2024-03-20 12:59:45 -0400

    add baseball game

---
+++ neet-code/stack/easy/baseball-game.js
@@ -0,0 +1,102 @@
+/*
+
+682. Baseball Game
+Easy
+Topics
+Companies
+You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+
+You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+
+An integer x.
+Record a new score of x.
+'+'.
+Record a new score that is the sum of the previous two scores.
+'D'.
+Record a new score that is the double of the previous score.
+'C'.
+Invalidate the previous score, removing it from the record.
+Return the sum of all the scores on the record after applying all the operations.
+
+The test cases are generated such that the answer and all intermediate calculations fit in a 32-bit integer and that all operations are valid.
+
+
+Example 1:
+Input: ops = ["5","2","C","D","+"]
+Output: 30
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"2" - Add 2 to the record, record is now [5, 2].
+"C" - Invalidate and remove the previous score, record is now [5].
+"D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+"+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+The total sum is 5 + 10 + 15 = 30.
+
+Example 2:
+Input: ops = ["5","-2","4","C","D","9","+","+"]
+Output: 27
+Explanation:
+"5" - Add 5 to the record, record is now [5].
+"-2" - Add -2 to the record, record is now [5, -2].
+"4" - Add 4 to the record, record is now [5, -2, 4].
+"C" - Invalidate and remove the previous score, record is now [5, -2].
+"D" - Add 2 * -2 = -4 to the record, record is now [5, -2, -4].
+"9" - Add 9 to the record, record is now [5, -2, -4, 9].
+"+" - Add -4 + 9 = 5 to the record, record is now [5, -2, -4, 9, 5].
+"+" - Add 9 + 5 = 14 to the record, record is now [5, -2, -4, 9, 5, 14].
+The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
+
+Example 3:
+Input: ops = ["1","C"]
+Output: 0
+Explanation:
+"1" - Add 1 to the record, record is now [1].
+"C" - Invalidate and remove the previous score, record is now [].
+Since the record is empty, the total sum is 0.
+
+
+Constraints:
+
+1 <= operations.length <= 1000
+operations[i] is "C", "D", "+", or a string representing an integer in the range [-3 * 104, 3 * 104].
+For operation "+", there will always be at least two previous scores on the record.
+For operations "C" and "D", there will always be at least one previous score on the record.
+*/
+
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function (operations) {
+  const stack = []
+
+  for (let i = 0; i < operations.length; i++) {
+    const operation = operations[i]
+
+    if (operation === '+') {
+      const peek = stack.pop()
+      const peek2 = stack.pop()
+      stack.push(peek2)
+      stack.push(peek)
+      stack.push(peek + peek2)
+    } else if (operation === 'C') {
+      stack.pop()
+    } else if (operation === 'D') {
+      const peek3 = stack.pop()
+      stack.push(peek3)
+      stack.push(peek3 * 2)
+    } else {
+      stack.push(Number(operation))
+    }
+  }
+
+  return stack.reduce((a, b) => a + b, 0)
+}
+
+const caseOne = ['5', '2', 'C', 'D', '+']
+const caseTwo = ['5', '-2', '4', 'C', 'D', '9', '+', '+']
+const caseThree = ['1', 'C']
+
+console.log(calPoints(caseOne)) // 30
+console.log(calPoints(caseTwo)) // 27
+console.log(calPoints(caseThree)) // 0
